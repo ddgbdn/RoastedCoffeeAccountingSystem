6ec6ca3b55259c2424b5fee46ddbecc89be7da98
P B <bogdan13531@gmail.com>
2023-02-20 01:59:25 +0600

    .

--- Shared/RequestFeatures/RoastingsParameters.cs
+++ Shared/RequestFeatures/RoastingsParameters.cs
@@ -8,5 +8,9 @@ namespace Shared.RequestFeatures
 {
     public class RoastingsParameters : RequestParameters
     {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; } = DateTime.Now;
+
+        public bool ValidDataRange => EndDate > StartDate;
     }
 }
